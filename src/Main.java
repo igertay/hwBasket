43c226f18cb86aa2403fb0b71841081d5ea1cc8e
Tata <lentstata@gmail.com>
2023-04-07 19:48:36 +0500

    первый коммит

---
+++ src/Main.java
@@ -0,0 +1,50 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+
+    static File saveFile = new File("basket.txt");
+
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] products = {"Хлебцы", "Кофе", "Авокадо"};
+        int[] prices = {40, 120, 100};
+        int[] prod = new int[3];
+        int productNumber = 0;
+        int productCount = 0;
+
+        Basket basket = null;
+        if (saveFile.exists()) {
+            basket = Basket.loadFromTxtFile(saveFile);
+        } else {
+            basket = new Basket(products, prices);
+        }
+
+        System.out.println("Перечень продуктов:");
+        for (int i = 0; i < products.length; i++) {
+            System.out.println((i + 1) + "." + products[i] + " " + prices[i] + " руб/шт.");
+        }
+        while (true) {
+
+            System.out.println("Выберите товар и количество или введите `end`");
+            String input = scanner.nextLine();
+
+            if (input.equals("end")) {
+                basket.printCart();
+                break;
+            } else {
+                String[] parts = input.split(" ");
+                productNumber = Integer.parseInt(parts[0]) - 1; // номер продукта
+                productCount = Integer.parseInt(parts[1]); //количество продукта
+                prod[productNumber] += productCount;
+                basket.addToCart(productNumber, productCount);
+                basket.saveTxt(saveFile);
+            }
+        }
+    }
+}
+
+
+
